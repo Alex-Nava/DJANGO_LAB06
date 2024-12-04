4edba6292668cffede8b3af621bbc620865720bf
Navarro <hnavarro@unsa.edu.pe>
2024-12-03 22:35:51 -0500

    Iniciar proyecto en Django

---
+++ VISTAS_TEMPLATES/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for VISTAS_TEMPLATES project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VISTAS_TEMPLATES.settings')
+
+application = get_asgi_application()
